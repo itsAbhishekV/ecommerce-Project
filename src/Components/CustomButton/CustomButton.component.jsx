6f82dfcdcb9387ad9a9b877c7a9802fc2ce7855b
Abhishek Verma <abhishekverma7t@gmail.com>
2021-06-22 20:24:14 +0530

    minor changes

--- src/Components/CustomButton/CustomButton.component.jsx
+++ src/Components/CustomButton/CustomButton.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './CustoButton.styles.scss';
 
-const CustomButton = ({children,inverted, isGoogleSignIn, ...otherButtonProps}) => (
+const CustomButton = ({children, inverted, isGoogleSignIn, ...otherButtonProps}) => (
         <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} ${inverted ? 'inverted' : ''} custom-button \`} custom-button `}  {...otherButtonProps} >
             {children}
         </button>
